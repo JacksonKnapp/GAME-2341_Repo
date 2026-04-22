3a982e69d5a3b0454b7a06dc4811698020a35840
Jackson Knapp <jackson.knapp0717@gmail.com>
2026-04-21 20:51:29 -0500

    Text_Adventure_3

--- PythonTextAdventure/Text Adventure.py
+++ PythonTextAdventure/Text Adventure.py
@@ -2,6 +2,7 @@
 import random
 import json
 
+# A function to validate input and make sure that an option that is chosen is valid
 def validate_input(options_result_dict, prompt):
     attempts = 5
     while True:
@@ -20,17 +21,56 @@ def validate_input(options_result_dict, prompt):
                 choice = random.choice(list(options_result_dict.values()))
                 return choice
             else:
-                attempts -= 5
+                attempts -= 1
                 print (f"That was not an available option! {attempts} attempts remaining until one is chosen for you!")
+
+# Removes all present console output and prints the provided prompt
+def flush_print(prompt):
+    print(prompt, end = "   ", flush = True)
+
+# Prints location details by a given location
+def print_location_details(location):
+    try:
+        #builds a string to pass onto a single print option that flushes the console
+        string = ""
+        desc = adventure["locations"][f"{location}"]["description"]
+        choices = adventure["locations"][f"{location}"]["choices"]
+        string = f"{location}:\n\n{desc}\n"
+        
+        for choice, info in choices.items():
+            desc = info["choice_description"]
+            dest = info["destination"]
+            string += f"\n{choice}\t| {desc}\t\tLeads to: {dest}"
+        flush_print(string)
+        return choices
+    except Exception as e:
+        print("Error: {e}")
 
 gameover = False
 
-# Loads adventure JSON file 
-with open ("adventure_tree.JSON", "r") as file:
-    adventure = json.load(file)
+# Loads adventure JSON file
+try: 
+    with open ("GAME-2341_Repo/PythonTextAdventure/adventure_tree.JSON", "r") as file:
+        adventure = json.load(file)
+except FileNotFoundError:
+    print("Error: adventure_tree.JSON file not found!")
+    gameover = True
 
 # Sets beginning parameters
-current_location = "start"
+current_location = "Start"
+print("\n\n\t============ Text Adventure ===========\n\n")
 
+# Game loop
 while gameover == False:
+    
+    choices = print_location_details(current_location)
 
+    choice = validate_input(choices,"\n\nChoice: ")
+
+
+
+
+
+
+# Game finished
+print("Game ended!")
