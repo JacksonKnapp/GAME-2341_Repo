938e3f0ef4961949cff1e62cec7e29392ca80141
JacksonKnapp <jackson.knapp0717@gmail.com>
2026-05-16 19:23:07 -0500

    Final_7

--- FinalProject/Main.py
+++ FinalProject/Main.py
@@ -121,6 +121,7 @@ pygame.display.set_caption("Reactor")
 tiles_group = pygame.sprite.Group()
 debug_messages = []
 animations = []
+tool_tip = None
 
 running = True # game loop variable
 
@@ -385,9 +386,9 @@ class UserInterface:
 
     def __init__(self):
         self.death_progress = ProgressBar(screen,640,22,735,26,DEATH_SPEED,(150,150,150)) #sets death_progress bar settings
-        self.construct_reactor_button = Button(100, 50, 160, 50, "Construct Reactor", font)
-        self.relocate_button = Button(50, 50, 160, 50, "Relocate", font)
-        self.recycle_tullium_button = Button(50, 50, 160, 50, "Recycle Tullium", font)
+        self.construct_reactor_button = Button(1055, 625, 160, 50, "Construct Reactor", font, "You need 10 Uranium to construct a reactor")
+        self.relocate_button = Button(1055, 530, 160, 50, "Relocate", font, "You need 5 Uranium to relocate")
+        self.recycle_tullium_button = Button(1055, 475, 160, 50, "Recycle Tullium", font, "You need 10 Tullium to recycle")
         self.construct_reactor_button.enabled = False
 
     def update_user_interface(self):
@@ -401,14 +402,18 @@ class UserInterface:
         self.relocate_button.draw(screen)
         self.recycle_tullium_button.draw(screen)
 
+        if tool_tip:
+            tool_tip.draw(screen)
+
     def reset_death_progress(self):
         self.death_progress.start(DEATH_SPEED)
 
 class Button:
      
-    def __init__(self, x, y, width, height, text, font):
+    def __init__(self, x, y, width, height, text, font, tooltip_text):
         self.rect = pygame.Rect(x, y, width, height)
         self.text = text
+        self.tooltip_text = tooltip_text
         self.font = font
         self.color = (80, 80, 80)
         self.hover_color = (120, 120, 120)
@@ -416,18 +421,32 @@ class Button:
         self.disabled_color = (255, 120, 120)
         self.disabled_hover_color = (200, 60, 60)
         self.enabled = True
+        self.ToolTip_object = None
 
     def draw(self, surface):
+
         mouse_pos = pygame.mouse.get_pos()
+        global tool_tip
 
         if self.rect.collidepoint(mouse_pos):
             if self.enabled:
                 color = self.hover_color
+                if self.ToolTip_object:
+                    del self.ToolTip_object
             else:
                 color = self.disabled_hover_color
+                if not self.ToolTip_object:
+                    x, y = pygame.mouse.get_pos()
+                    self.ToolTip_object = ToolTip(x, y, 450, 50, self.tooltip_text, font)
+                else:
+                    tool_tip = self.ToolTip_object
         else:
+            if self.ToolTip_object:
+                    self.ToolTip_object = None
+                    tool_tip = None
             if self.enabled:
                 color = self.color
+                
             else:
                 color = self.disabled_color
 
@@ -442,6 +461,27 @@ class Button:
             and event.button == 1
             and self.rest.collidepoint(event.pos)
         )
+
+class ToolTip:
+
+    def __init__(self, x, y, width, height, text, font):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.font = font
+        self.color = (60, 60, 60)
+        self.text_color = (255, 255, 255)
+
+    def draw(self, surface):
+        x, y = pygame.mouse.get_pos()
+        y -= 25 
+        mouse_pos = (x,y)
+
+        self.rect.bottomright = mouse_pos
+
+        pygame.draw.rect(surface, self.color, self.rect, border_radius=8)
+        text_surface = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        surface.blit(text_surface, text_rect)
 
     
 ################### MAIN ##################
